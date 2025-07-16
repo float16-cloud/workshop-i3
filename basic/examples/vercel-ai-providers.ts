cc4b9da2eac661bff4a64ac4d067c89bf18a4a44
bossthanawat <boss41680@gmail.com>
2025-07-16 18:02:38 +0700

    เพิ่มการสนับสนุนสำหรับ Azure SDK ในไฟล์ package.json, bun.lock และตัวอย่างการใช้งาน รวมถึงการปรับปรุงไฟล์ .env.example เพื่อรองรับการตั้งค่าของ Azure

--- basic/examples/vercel-ai-providers.ts
+++ basic/examples/vercel-ai-providers.ts
@@ -4,18 +4,25 @@ import { generateText } from "ai";
 import { createOpenAI } from "@ai-sdk/openai";
 /* other providers **/
 // import { anthropic } from '@ai-sdk/anthropic';
-// import { azure } from '@ai-sdk/azure';
+import { createAzure } from '@ai-sdk/azure';
 
-const openai = createOpenAI({
-  baseURL: process.env.OPENAI_BASE_API,
-  apiKey: process.env.OPENAI_API_KEY,
-  compatibility: 'compatible',
-});
+// const openai = createOpenAI({
+//   baseURL: process.env.OPENAI_BASE_API,
+//   apiKey: process.env.OPENAI_API_KEY,
+//   compatibility: 'compatible',
+// });
+
+export const azure =  createAzure({
+  baseURL: process.env.AZURE_BASE_API,
+  apiKey: process.env.AZURE_API_KEY,
+  apiVersion: process.env.API_VERSION
+})
 
 
 const { text } = await generateText({
-  model: openai("gpt-4o-mini"),
+  // model: openai("gpt-4o-mini"),
   // model: anthropic("claude-3-5-sonnet-20240620"), 
+  model: azure("gpt-4o-mini"),
   messages: [
     {
       role: "system",
